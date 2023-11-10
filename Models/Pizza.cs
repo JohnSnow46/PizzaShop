789b6f28bb6dfd1629618839587834fe70e2d4e8
Frank <franekuna@gmail.com>
2023-11-10 18:49:23 +0100

    Create Pizza.cs

---
+++ Models/Pizza.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaShop.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Description { get; set; }
+    }
+}
