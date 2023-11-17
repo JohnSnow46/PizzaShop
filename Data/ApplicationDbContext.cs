e2fa09bc50e116de53914b1fdd6b268546d261fb
Frank <franekuna@gmail.com>
2023-11-17 15:59:04 +0100

    temp

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -18,8 +18,8 @@ namespace PizzaShop.Data
                 new Pizza { Id = 1, Name = "Hawajska", Description = "Pizza Hawajska" },
                 new Pizza { Id = 2, Name = "Capri", Description = "Pizza Capri" },
                 new Pizza { Id = 3, Name = "Boczek", Description = "Pizza Boczek" },
-                new Pizza { Id = 3, Name = "Margarita", Description = "Pizza margarita" },
-                new Pizza { Id = 3, Name = "Cztery Sery", Description = "Pizza Cztery Sery" }
+                new Pizza { Id = 4, Name = "Margarita", Description = "Pizza margarita" },
+                new Pizza { Id = 5, Name = "Cztery Sery", Description = "Pizza Cztery Sery" }
                 );
             base.OnModelCreating(modelBuilder);
         }
---
+++ Migrations/20231117130003_NewPositionsInMenu.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PizzaShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPositionsInMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Pizza",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 4, "Pizza margarita", "Margarita" },
+                    { 5, "Pizza Cztery Sery", "Cztery Sery" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pizza",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Pizza",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
